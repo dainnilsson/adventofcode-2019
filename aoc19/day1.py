f47c17a34e360b51fcd57b0aacd5b20b88dbe681
Dain Nilsson <dain@yubico.com>
2019-12-01 09:11:21 +0100

    Day 1.

--- aoc19/day1.py
+++ aoc19/day1.py
@@ -1,17 +1,12 @@
-import fileinput
-
-"""Day 1 from AOC 2017 as a sample."""
-
-
 def solve(lines):
-    ds = [int(d) for d in lines[0]]
-    n = len(ds)
-
-    return (
-        sum(d for i, d in enumerate(ds) if d == ds[(i + 1) % n]),
-        sum(d for i, d in enumerate(ds) if d == ds[(i + n // 2) % n]),
-    )
+    ms = [[int(d) // 3 - 2] for d in lines]
+    for m in ms:
+        while m[-1]:
+            m.append(max(0, m[-1] // 3 - 2))
+    return (sum(l[0] for l in ms), sum(sum(l) for l in ms))
 
 
 if __name__ == "__main__":
+    import fileinput
+
     print(solve([l.rstrip("\r\n") for l in fileinput.input()]))
