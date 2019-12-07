949aabdb62bf7515adf56818e2ea4a7e26633272
Dain Nilsson <dain@yubico.com>
2019-12-07 19:46:06 +0100

    Day 7: Optimized.

--- aoc19/day7.py
+++ aoc19/day7.py
@@ -49,6 +49,41 @@ def solve(lines):
     )
 
 
+def solve_b_opt(data):
+    qs = [Queue() for _ in range(5)]
+    qs.append(qs[0])
+    ps = [Program(data, qs[i].get, qs[i + 1].put) for i in range(5)]
+
+    b = 0
+    for seqs in permutations(range(5, 10), 5):
+        for i, s in enumerate(seqs):
+            qs[i].put(s)
+        qs[0].put(0)
+
+        for p in ps:
+            p.state = p.data.copy()
+            p.pc = 0
+
+        act = 0  # Do our own scheduling
+        op = ps[0].step()
+        while op != 99:
+            if op == 4:
+                act = (act + 1) % 5
+            op = ps[act].step()
+
+        b = max(b, qs[0].get())
+    return b
+
+
+def optimized(lines):
+    data = [int(d) for d in lines[0].split(",")]
+
+    return (
+        solve_a(data),
+        solve_b_opt(data)
+    )
+
+
 if __name__ == "__main__":
     import fileinput
 
