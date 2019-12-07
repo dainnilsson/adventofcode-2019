8d74dbec79e8b9828a8fc507125240b4ec394fcc
Dain Nilsson <dain@yubico.com>
2019-12-07 15:20:22 +0100

    Day 7.

---
+++ aoc19/day7.py
@@ -0,0 +1,55 @@
+from .day5 import Program
+
+from queue import Queue
+from itertools import permutations
+from concurrent.futures import ThreadPoolExecutor
+
+
+def solve_a(data):
+    qs = [Queue() for _ in range(6)]
+    ps = [Program(data, qs[i].get, qs[i + 1].put) for i in range(5)]
+
+    a = 0
+    for seqs in permutations(range(5), 5):
+        for i, s in enumerate(seqs):
+            qs[i].put(s)
+        qs[0].put(0)
+
+        for p in ps:
+            p.run()
+        a = max(a, qs[-1].get())
+    return a
+
+
+def solve_b(data):
+    qs = [Queue() for _ in range(5)]
+    qs.append(qs[0])
+    ps = [Program(data, qs[i].get, qs[i + 1].put) for i in range(5)]
+
+    b = 0
+    for seqs in permutations(range(5, 10), 5):
+        for i, s in enumerate(seqs):
+            qs[i].put(s)
+        qs[0].put(0)
+
+        with ThreadPoolExecutor() as e:
+            for p in ps:
+                e.submit(p.run)
+
+        b = max(b, qs[0].get())
+    return b
+
+
+def solve(lines):
+    data = [int(d) for d in lines[0].split(",")]
+
+    return (
+        solve_a(data),
+        solve_b(data)
+    )
+
+
+if __name__ == "__main__":
+    import fileinput
+
+    print(solve([l.rstrip("\r\n") for l in fileinput.input()]))
