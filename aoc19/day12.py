73171128f0ae957f10eb3d8da08df8461c07578e
Dain Nilsson <dain@yubico.com>
2019-12-12 13:49:00 +0100

    Remove "optimized".

--- aoc19/day12.py
+++ aoc19/day12.py
@@ -77,51 +77,6 @@ def solve(lines):
     return a, b
 
 
-def optimized(lines):
-    moons = []
-    for line in lines:
-        match = MOON.match(line)
-        moons.append(([int(match.group(i)) for i in range(1, 4)], [0, 0, 0]))
-
-    t = 0
-    ans_a = 0
-    seen = [set(), set(), set()]
-    done = [False, False, False]
-    while True:
-        for i in range(3):
-            if not done[i]:
-                state = tuple((m[0][i], m[1][i]) for m in moons)
-                if state in seen[i]:
-                    done[i] = t
-                    if all(done):
-                        x, y, z = done
-                        xy = x * y // gcd(x, y)
-                        ans_b = xy * z // gcd(xy, z)
-                        return ans_a, ans_b
-                else:
-                    seen[i].add(state)
-
-        t += 1
-
-        for a, b in combinations(moons, 2):
-            for i in range(3):
-                if a[0][i] < b[0][i]:
-                    a[1][i] += 1
-                    b[1][i] -= 1
-                elif b[0][i] < a[0][i]:
-                    a[1][i] -= 1
-                    b[1][i] += 1
-
-        for m in moons:
-            for i in range(3):
-                m[0][i] += m[1][i]
-
-        if t == 1000:
-            ans_a = sum(
-                sum(abs(c) for c in m[0]) * sum(abs(c) for c in m[1]) for m in moons
-            )
-
-
 if __name__ == "__main__":
     import fileinput
 
